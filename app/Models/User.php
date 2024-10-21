aeafab64a2c1244eafdeba5752f2d2b00b1ee11c
thomas-d <tho.daskalakis@gmail.com>
2024-10-21 05:14:50 +0300

    add missing properties to user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'timezone',
     ];
 
     /**
@@ -31,6 +32,7 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
+        'email_verified_at',
     ];
 
     /**
